eb2c5b1dac65b7849814ebd0c16255b83c466603
Yusuf Lawal <yusuflawalabdullahi26@gmail.com>
2024-09-08 14:25:09 +0100

    Impliment Auth, appwrite and sentry

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,17 +1,21 @@
 import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-const loggedIn = {firstName: 'Yusuf', LastName: 'Dev'};
+  const loggedIn = await getLoggedInUser();
+
+  if(!loggedIn) redirect('/sign-in')
 
   return (
     <main className="flex h-screen w-full font-inter">
-      <Sidebar user={loggedIn} />
+      <Sidebar user ={loggedIn} />
 
       <div className="flex size-full flex-col">
         <div className="root-layout">
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,12 @@
 import HeaderBox from '@/components/HeaderBox'
 import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
+import { getLoggedInUser } from '@/lib/actions/user.actions'
 import React from 'react'
 
 
-const Home = () => {
-  const loggedIn = {firstName: 'Yusuf', lastName:'Lawal', email: 'yusuflawalabdullahi26@gmail.com'}
+const Home = async () => {
+  const loggedIn = await getLoggedInUser();
   return (
     <section className='home'>
       <div className='home-content'>
@@ -13,7 +14,7 @@ const Home = () => {
           <HeaderBox 
             type="greeting"
             title="Welcome"
-            user={loggedIn?.firstName || 'Guest'}
+            user={loggedIn?.name || 'Guest'}
             subtext="Access and manage your account and transactions efficiently."
           />
           <TotalBalanceBox 
